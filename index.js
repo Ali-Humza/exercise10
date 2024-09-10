c40d3804f1f266f5b46767a208b038a36cf33c03
Ali-humza <lexoff2023@gmail.com>
2024-09-10 08:16:14 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// let adjectives =
+// {
+//   key1: "Crazy " , 
+//   key2: "Amazing " ,
+//   key3: "Fire ",    
+// };
+
+// let ShopName =
+//   {
+//     key1: "Engine " , 
+//     key2: "Food " ,
+//     key3: "Electronics ",
+//   };
+
+// let Another = 
+//   {
+//     key1: "corner" ,
+//     key2: "centre" ,
+//     key3: "Hub"
+//   };
+
+// function random(obj)
+// {
+//   let values = Object.values(obj);
+//   let randomIndex = Math.floor(Math.random() * values.length);
+//   return values[randomIndex];
+// }
+
+// let rnd1 = random(adjectives);
+// let rnd2 = random(ShopName);
+// let rnd3 = random(Another);
+  
+// console.log(rnd1 + rnd2 + rnd3)
+
+// function average(a,b) {
+//   console.log((a + b)/2);
+
+// }
+
+// average(12,10)
+
+let a = "ali";
+for (let i =6; i < 19 ; i++){
+console.log(a+i)
+}
